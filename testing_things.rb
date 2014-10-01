ef53f1cc3ef3f65ba0da0cf86d2c42df808f8c1f
Steve Loveless <steve@agrian.com>
2014-09-30 23:59:17 -0700

    Added another tif for testing

--- testing_things.rb
+++ testing_things.rb
@@ -8,6 +8,9 @@ proj = 'MULTIPOLYGON (((-120.03296317083633 44.226828996384285, -120.03199741570
 harper_path = '/Users/sloveless/Development/projects/ffi-gdal/spec/support/images/Harper/Harper_1058_20140612_NRGB.tif'
 harper = GDAL::Dataset.open(harper_path, 'r')
 
+floyd_path = '/Users/sloveless/Development/projects/ffi-gdal/spec/support/images/Floyd/Floyd_1058_20140612_NRGB.tif'
+floyd = GDAL::Dataset.open(floyd_path, 'r')
+
 usg_path = '/Users/sloveless/Development/projects/ffi-gdal/spec/support/images/osgeo/geotiff/usgs/c41078a1.tif'
 usg = GDAL::Dataset.open(usg_path, 'r')
 
