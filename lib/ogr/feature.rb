738705c8f0f82f47389945dce64a077b102397a6
Steve Loveless <steve@agrian.com>
2014-10-03 03:43:24 -0700

    More feature & feature defn wrapping

--- lib/ogr/feature.rb
+++ lib/ogr/feature.rb
@@ -6,9 +6,12 @@ module OGR
   class Feature
     include FFI::GDAL
 
-    def initialize(ogr_feature_pointer: nil)
+    # @param feature_definition [OGR::FeatureDefinition]
+    def initialize(feature_defintion, ogr_feature_pointer: nil)
       @ogr_feature_pointer = if ogr_feature_pointer
         ogr_feature_pointer
+      else
+        OGR_F_Create(field_definition.c_pointer)
       end
 
       close_me = -> { FFI::GDAL.OGR_F_Destroy(@ogr_feature_pointer) }
@@ -50,12 +53,17 @@ module OGR
       OGR_F_GetRawFieldRef(@ogr_feature_pointer, index)
     end
 
+    # @param index [Fixnum]
+    def unset_field(index)
+      OGR_F_UnsetField(@ogr_feature_pointer, index)
+    end
+
     # @return [OGR::FeatureDefinition,nil]
     def definition
       feature_defn_ptr = OGR_F_GetDefRef(@ogr_feature_pointer)
       return nil if feature_defn_ptr.null?
 
-      OGR::FeatureDefinition.new(ogr_feature_defn_pointer: feature_defn_ptr)
+      OGR::FeatureDefinition.new('', ogr_feature_defn_pointer: feature_defn_ptr)
     end
 
     # @return [OGR::Geometry]
@@ -66,11 +74,76 @@ module OGR
       OGR::Geometry.new(ogr_geometry_pointer: geometry_ptr)
     end
 
+    # @param new_geometry [OGR::Geometry]
+    def geometry=(new_geometry)
+      ogr_err = OGR_F_SetGeometry(@ogr_feature_pointer, new_geometry.c_pointer)
+    end
+
+    # @return [Fixnum]
+    def geometry_field_count
+      OGR_F_GetGeomFieldCount(@ogr_feature_pointer)
+    end
+
     # @return [Boolean]
     def equal?(other_feature)
       OGR_F_Equal(@ogr_feature_pointer, feature_pointer_from(other_feature))
     end
     alias_method :equals?, :equal?
+
+    # @param index [Fixnum]
+    # @return [Fixnum]
+    def field_as_integer(index)
+      OGR_F_GetFieldAsInteger(@ogr_feature_pointer, index)
+    end
+
+    # @param index [Fixnum]
+    # @return [Array<Fixnum>]
+    def field_as_integer_list(index)
+      count_ptr = FFI::MemoryPointer.new(:int)
+      list_ints = OGR_F_GetFieldAsIntegerList(@ogr_feature_pointer, index, count_ptr)
+
+      list_inst.read_array_of_int
+    end
+
+    # @param index [Fixnum]
+    # @return [Float]
+    def field_as_double(index)
+      OGR_F_GetFieldAsDouble(@ogr_feature_pointer, index)
+    end
+
+    # @param index [Fixnum]
+    # @return [Array<Float>]
+    def field_as_double_list(index)
+      count_ptr = FFI::MemoryPointer.new(:int)
+      list_ints = OGR_F_GetFieldAsDoubleList(@ogr_feature_pointer, index, count_ptr)
+
+      list_inst.read_array_of_double
+    end
+
+    # @param index [Fixnum]
+    # @return [String]
+    def field_as_string(index)
+      OGR_F_GetFieldAsString(@ogr_feature_pointer, index)
+    end
+
+    # @param index [Fixnum]
+    # @return [Array<String>]
+    def field_as_string_list(index)
+      count_ptr = FFI::MemoryPointer.new(:int)
+      list_ints = OGR_F_GetFieldAsStringList(@ogr_feature_pointer, index, count_ptr)
+
+      list_inst.read_array_of_string
+    end
+
+    # @return [String]
+    def style_string
+      OGR_F_GetStyleString(@ogr_feature_pointer)
+    end
+
+    # @param new_style [String]
+    def style_string=(new_style)
+      OGR_F_SetStyleString(@ogr_feature_pointer, new_style)
+    end
 
     private
 
--- lib/ogr/feature_definition.rb
+++ lib/ogr/feature_definition.rb
@@ -1,17 +1,119 @@
 require_relative '../ffi/ogr'
+require_relative 'field_definition'
 
 module OGR
   class FeatureDefinition
     include FFI::GDAL
 
-    def initialize(ogr_feature_defn_pointer: nil)
+    # @param name [String]
+    def initialize(name, ogr_feature_defn_pointer: nil)
       @ogr_feature_defn_pointer = if ogr_feature_defn_pointer
         ogr_feature_defn_pointer
+      else
+        OGR_FD_Create(name)
       end
+
+      close_me = -> { FFI::GDAL.OGR_FD_Destroy(@ogr_feature_defn_pointer) }
+      ObjectSpace.define_finalizer self, close_me
     end
 
     def c_pointer
       @ogr_feature_defn_pointer
     end
+
+    # @return [String]
+    def name
+      OGR_FD_GetName(@ogr_feature_defn_pointer)
+    end
+
+    # @return [Fixnum]
+    def field_count
+      OGR_FD_GetFieldCount(@ogr_feature_defn_pointer)
+    end
+
+    # @param index [Fixnum]
+    # @return [OGR::FieldDefinition]
+    def field_definition(index)
+      field_def_ptr = OGR_FD_GetFieldDefn(@ogr_feature_defn_pointer)
+      return nil if field_def_ptr.null?
+
+      OGR::FieldDefinition.new(ogr_field_defn_pointer: field_def_ptr)
+    end
+
+    # @param name [String]
+    # @return [Fixnum] -1 if no match found
+    def field_index(name)
+      OGR_FD_GetFieldIndex(@ogr_feature_defn_pointer, name)
+    end
+
+    # @param name [String]
+    # @return [OGR::FieldDefinition]
+    def field_by_name(name)
+      field_definition(field_index(name))
+    end
+
+    # @return [FFI::GDAL::OGRwkbGeometryType]
+    def geometry_type
+      OGR_FD_GetGeomType(@ogr_feature_defn_pointer)
+    end
+
+    # @return [Boolean]
+    def geometry_ignored?
+      OGR_FD_IsGeometryIgnored(@ogr_feature_defn_pointer)
+    end
+
+    # @param ignore [Boolean]
+    def ignore_geometry!(ignore)
+      OGR_FD_SetGeometryIgnored(@ogr_feature_defn_pointer, ignore)
+    end
+
+    # @return [Boolean]
+    def style_ignored?
+      OGR_FD_IsStyleIgnored?(@ogr_feature_defn_pointer)
+    end
+
+    # @param ignore [Boolean]
+    def ignore_style!(ignore)
+      OGR_FD_SetStyleIgnored(@ogr_feature_defn_pointer, ignore)
+    end
+
+    # @return [Fixnum]
+    def geometry_field_count
+      OGR_FD_GetGeomFieldCount(@ogr_feature_defn_pointer)
+    end
+
+    # @param index [Fixnum]
+    # @return [OGR::FieldDefinition]
+    def geometry_field_definition(index)
+      fd_ptr = OGR_FD_GetGeomFieldDefn(@ogr_feature_defn_pointer, index)
+      return nil if fd_ptr.null?
+
+      OGR::FieldDefinition.new(ogr_field_defn_pointer: fd_ptr)
+    end
+
+    # @param name [String]
+    # @return [Fixnum]
+    def geometry_field_index(name)
+      OGR_FD_GetGeomFieldIndex(@ogr_feature_defn_pointer, name)
+    end
+
+    # @param name [String]
+    # @return [OGR::FieldDefinition]
+    def geometry_field_by_name(name)
+      geometry_field_definition(geometry_field_index(name))
+    end
+
+    # @param other_field_defintion [OGR::FieldDefinition, FFI::Pointer]
+    # @return [Boolean]
+    def same?(other_field_defintion)
+      fd_ptr = if other_field_defintion.is_a? OGR::FieldDefinition
+        other_field_defintion.c_pointer
+      else
+        other_field_defintion
+      end
+
+      OGR_FD_IsSame(@ogr_feature_defn_pointer, fd_ptr)
+    end
+    alias_method, :==, :same?
   end
 end
