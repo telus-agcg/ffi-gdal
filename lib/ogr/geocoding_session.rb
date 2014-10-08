5b77b02d131d65c7fb56c05adb23327d23d1e1d1
Steve Loveless <steve@agrian.com>
2014-10-08 09:58:47 -0700

    Added docs

--- lib/ogr/geocoding_session.rb
+++ lib/ogr/geocoding_session.rb
@@ -4,8 +4,33 @@ module OGR
 
   # Geocode things!  http://www.gdal.org/ogr__geocoding_8h.html
   class GeocodingSession
+    # @param options [Hash]
+    # @option options cache_file [String] Name of the file to write the cache
+    #   to.
+    # @option options read_cache [Boolean] Defaults to true.
+    # @option options write_cache [Boolean] Defaults to true.
+    # @option options service [String] "MAPQUEST_NOMINATIM", "YAHOO",
+    #   "GEONAMES", "BING", or other value.
+    # @option options email [String] Required for the GEONAMES service.
+    # @option options key [String] Required for the BING service.
+    # @option options application [String] Sets the User-Agent request header.
+    #   Defaults to the GDAL version string.
+    # @option options language [String] Sets the Accept-Language request
+    #   headeer.
+    # @option options delay [Float] Minimum delay, in seconds, between
+    #   consecutive requests. Defaults to 1.0.
+    # @option options query_template [String] URL template for GET requests.
+    #   Must contain one and only one occurence of %s.  If not specified, the
+    #   URL template is hard-coded.
+    # @option options reverse_query_template [String] Template to use for
+    #   reverse geocoding.
     def initialize(**options)
-      options_ptr = GDAL::Options.pointer(options)
+      converted_options = options.each_with_object({}) do |(k, v), obj|
+        key = "OGR_GEOCODE_#{k.to_s.upcase}"
+        obj[key] = v
+      end
+
+      options_ptr = GDAL::Options.pointer(converted_options)
 
       @geocoding_session_pointer = FFI::GDAL.OGRGeocodeCreateSession(options_ptr)
     end
@@ -19,6 +44,16 @@ module OGR
     end
 
     # @param query [String]
+    # @param options [Hash]
+    # @option options addressdetails [Boolean] +true+ to include a breakdown of
+    #   the adddress into elements.  Only works with some geocoding services.
+    # @option options countrycodes [Array<String, Symbol>] Limit the search to a
+    #   specific country or countries.  Only works with some geocoding services.
+    # @option options limit [Fixnum] Limit the number of records returned.  Only
+    #   works with some geocoding services.
+    # @option options raw_feature ["YES"] Adds a raw feature to the returned
+    #   feature that includes that raw XML response body.
+    # @option extra_query_parameters [String] Adds params to the GET request.
     # @return [OGR::Layer, nil]
     def geocode(query, **options)
       options_ptr = GDAL::Options.pointer(options)
@@ -31,6 +66,11 @@ module OGR
 
     # @param lon [Float]
     # @param lat [Float]
+    # @param options [Hash]
+    # @option options zoom [Float] Only used by the Nominatim service.
+    # @option options raw_feature ["YES"] Adds a raw feature to the returned
+    #   feature that includes that raw XML response body.
+    # @option extra_query_parameters [String] Adds params to the GET request.
     # @return [OGR::Layer]
     def reverse_geocode(lon, lat, **options)
       options_ptr = GDAL::Options.pointer(options)
