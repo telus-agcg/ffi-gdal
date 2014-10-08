bb32e151e7e91adb070047d43055eb0bbab01f9d
Steve Loveless <steve@agrian.com>
2014-10-07 22:18:11 -0700

    Autoload API classes/modules

--- lib/ffi-gdal.rb
+++ lib/ffi-gdal.rb
@@ -7,12 +7,28 @@ module GDAL
   extend EnvironmentMethods
   extend LogSwitch
 
+  autoload :ColorEntry,
+    File.expand_path('gdal/color_entry', __dir__)
   autoload :ColorInterpretation,
     File.expand_path('gdal/color_interpretation', __dir__)
+  autoload :ColorTable,
+    File.expand_path('gdal/color_table', __dir__)
   autoload :DataType,
     File.expand_path('gdal/data_type', __dir__)
+  autoload :Dataset,
+    File.expand_path('gdal/dataset', __dir__)
+  autoload :Driver,
+    File.expand_path('gdal/driver', __dir__)
+  autoload :GeoTransform,
+    File.expand_path('gdal/geo_transform', __dir__)
+  autoload :MajorObject,
+    File.expand_path('gdal/major_object', __dir__)
   autoload :Options,
     File.expand_path('gdal/options', __dir__)
+  autoload :RasterAttributeTable,
+    File.expand_path('gdal/raster_attribute_table', __dir__)
+  autoload :Utils,
+    File.expand_path('gdal/utils', __dir__)
 
   # Register all drivers!
   FFI::GDAL.GDALAllRegister
@@ -20,6 +36,25 @@ end
 
 module OGR
   extend LogSwitch
+
+  autoload :DataSource,
+    File.expand_path('ogr/data_source', __dir__)
+  autoload :Driver,
+    File.expand_path('ogr/driver', __dir__)
+  autoload :Envelope,
+    File.expand_path('ogr/envelope', __dir__)
+  autoload :Feature,
+    File.expand_path('ogr/feature', __dir__)
+  autoload :FeatureDefinition,
+    File.expand_path('ogr/feature_definition', __dir__)
+  autoload :Geometry,
+    File.expand_path('ogr/geometry', __dir__)
+  autoload :Layer,
+    File.expand_path('ogr/layer', __dir__)
+  autoload :SpatialReference,
+    File.expand_path('ogr/spatial_reference', __dir__)
+  autoload :StyleTable,
+    File.expand_path('ogr/style_table', __dir__)
 end
 
 GDAL.log_class_name = true
@@ -28,5 +63,3 @@ OGR.log_class_name = true
 require_relative 'ffi/gdal'
 require_relative 'ffi/ogr'
 require_relative 'ext/float_ext'
-require_relative 'gdal/dataset'
-require_relative 'ogr/data_source'
