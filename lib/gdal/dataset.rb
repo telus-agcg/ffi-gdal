88d7b4023dcbfbf8ccd0a046f1e7bed591813c05
Steve Loveless <steve@agrian.com>
2014-10-10 01:56:06 -0700

    Initial add of #rasterize_layers

--- lib/gdal/dataset.rb
+++ lib/gdal/dataset.rb
@@ -352,6 +352,16 @@ module GDAL
     # @param band_numbers [Array<Fixnum>, Fixnum]
     # @param geometries [Array<OGR::Geometry>, OGR::Geometry]
     # @param burn_values [Array<Float>, Float]
+    # @param transformer [Proc]
+    # @param options [Hash]
+    # @option options all_touched [Boolean]  If +true+, sets all pixels touched
+    #   by the line or polygons, not just those whose center is within the
+    #   polygon or that are selected by Brezenham's line algorithm.  Defaults to
+    #   +false+.
+    # @option options burn_value_from ["Z"] Use the Z values of the geometries.
+    # @option @options merge_alg [String] "REPLACE" or "ADD".  REPLACE results
+    #   in overwriting of value, while ADD adds the new value to the existing
+    #   raster, suitable for heatmaps for instance.
     def rasterize_geometries(band_numbers, geometries, burn_values,
       transformer: nil, transform_arg: nil, **options, &progress_block)
       gdal_options = GDAL::Options.pointer(options)
@@ -382,6 +392,62 @@ module GDAL
         gdal_options,
         progress_block,
         progress_callback_data)
+
+      cpl_err.to_bool
+    end
+
+    # @param band_numbers [Array<Fixnum>, Fixnum]
+    # @param layers [Array<OGR::Layer>, OGR::Layer]
+    # @param burn_values [Array<Float>, Float]
+    # @param transformer [Proc]
+    # @param options [Hash]
+    # @option options attribute [String] An attribute field on features to be
+    #   used for a burn-in value, which will be burned into all output bands.
+    # @option options chunkysize [Fixnum] The height in lines of the chunk to
+    #   operate on.
+    # @option options all_touched [Boolean]  If +true+, sets all pixels touched
+    #   by the line or polygons, not just those whose center is within the
+    #   polygon or that are selected by Brezenham's line algorithm.  Defaults to
+    #   +false+.
+    # @option options burn_value_from ["Z"] Use the Z values of the geometries.
+    # @option @options merge_alg [String] "REPLACE" or "ADD".  REPLACE results
+    #   in overwriting of value, while ADD adds the new value to the existing
+    #   raster, suitable for heatmaps for instance.
+    def rasterize_layers(band_numbers, layers, burn_values,
+      transformer: nil, transform_arg: nil, **options, &progress_block)
+      gdal_options = GDAL::Options.pointer(options)
+      band_numbers = band_numbers.is_a?(Array) ? band_numbers : [band_numbers]
+      log "band numbers: #{band_numbers}"
+
+      layers = layers.is_a?(Array) ? layers : [layers]
+      log "layers: #{layers}"
+
+      burn_values = burn_values.is_a?(Array) ? burn_values : [burn_values]
+      log "burn values: #{burn_values}"
+
+      band_numbers_ptr = FFI::MemoryPointer.new(:pointer, band_numbers.size)
+      band_numbers_ptr.write_array_of_int(band_numbers)
+      log "band numbers ptr null? #{band_numbers_ptr.null?}"
+
+      layers_ptr = FFI::MemoryPointer.new(:pointer, layers.size)
+      layers_ptr.write_array_of_pointer(layers.map(&:c_pointer))
+      log "layers ptr null? #{layers_ptr.null?}"
+
+      burn_values_ptr = FFI::MemoryPointer.new(:pointer, burn_values.size)
+      burn_values_ptr.write_array_of_double(burn_values)
+      log "burn value ptr null? #{burn_values_ptr.null?}"
+
+      cpl_err = GDALRasterizeLayers(@dataset_pointer,     # hDS
+        band_numbers.size,                                # nBandCount
+        band_numbers_ptr,                                 # panBandList
+        layers.size,                                      # nLayerCount
+        layers_ptr,                                       # pahLayers
+        transformer,                                      # pfnTransformer
+        transform_arg,                                    # pTransformerArg
+        burn_values_ptr,                                  # padfLayerBurnValues
+        gdal_options,                                     # papszOptions
+        progress_block,                                   # pfnProgress
+        nil)                                              # pProgressArg
 
       cpl_err.to_bool
     end
