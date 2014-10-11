5017ff28bc5edf70d0ecb60be2fa6e433ecfc48d
Steve Loveless <steve@agrian.com>
2014-10-10 21:45:54 -0700

    No more FFI::GDAL mixing in

--- lib/ogr/feature_definition.rb
+++ lib/ogr/feature_definition.rb
@@ -3,7 +3,6 @@ require_relative 'field'
 
 module OGR
   class FeatureDefinition
-    include FFI::GDAL
 
     # @param name [String]
     def self.create(name)
@@ -28,18 +27,18 @@ module OGR
 
     # @return [String]
     def name
-      OGR_FD_GetName(@feature_definition_pointer)
+      FFI::GDAL.OGR_FD_GetName(@feature_definition_pointer)
     end
 
     # @return [Fixnum]
     def field_count
-      OGR_FD_GetFieldCount(@feature_definition_pointer)
+      FFI::GDAL.OGR_FD_GetFieldCount(@feature_definition_pointer)
     end
 
     # @param index [Fixnum]
     # @return [OGR::Field]
     def field(index)
-      field_ptr = OGR_FD_GetFieldDefn(@feature_definition_pointer)
+      field_ptr = FFI::GDAL.OGR_FD_GetFieldDefn(@feature_definition_pointer)
       return nil if field_ptr.null?
 
       OGR::Field.new(field_ptr)
@@ -48,7 +47,7 @@ module OGR
     # @param name [String]
     # @return [Fixnum] -1 if no match found
     def field_index(name)
-      OGR_FD_GetFieldIndex(@feature_definition_pointer, name)
+      FFI::GDAL.OGR_FD_GetFieldIndex(@feature_definition_pointer, name)
     end
 
     # @param name [String]
@@ -59,43 +58,43 @@ module OGR
 
     # @return [FFI::GDAL::OGRwkbGeometryType]
     def geometry_type
-      OGR_FD_GetGeomType(@feature_definition_pointer)
+      FFI::GDAL.OGR_FD_GetGeomType(@feature_definition_pointer)
     end
 
     # @param new_type [FFI::GDAL::OGRwkbGeometryType]
     def geometry_type=(new_type)
-      OGR_FD_SetGeomType(@feature_definition_pointer, new_type)
+      FFI::GDAL.OGR_FD_SetGeomType(@feature_definition_pointer, new_type)
     end
 
     # @return [Boolean]
     def geometry_ignored?
-      OGR_FD_IsGeometryIgnored(@feature_definition_pointer)
+      FFI::GDAL.OGR_FD_IsGeometryIgnored(@feature_definition_pointer)
     end
 
     # @param ignore [Boolean]
     def ignore_geometry!(ignore)
-      OGR_FD_SetGeometryIgnored(@feature_definition_pointer, ignore)
+      FFI::GDAL.OGR_FD_SetGeometryIgnored(@feature_definition_pointer, ignore)
     end
 
     # @return [Boolean]
     def style_ignored?
-      OGR_FD_IsStyleIgnored?(@feature_definition_pointer)
+      FFI::GDAL.OGR_FD_IsStyleIgnored?(@feature_definition_pointer)
     end
 
     # @param ignore [Boolean]
     def ignore_style!(ignore)
-      OGR_FD_SetStyleIgnored(@feature_definition_pointer, ignore)
+      FFI::GDAL.OGR_FD_SetStyleIgnored(@feature_definition_pointer, ignore)
     end
 
     # @return [Fixnum]
     def geometry_field_count
-      OGR_FD_GetGeomFieldCount(@feature_definition_pointer)
+      FFI::GDAL.OGR_FD_GetGeomFieldCount(@feature_definition_pointer)
     end
 
     # @param index [Fixnum]
     # @return [OGR::Field]
     def geometry_field_definition(index)
-      field_ptr = OGR_FD_GetGeomFieldDefn(@feature_definition_pointer, index)
+      field_ptr = FFI::GDAL.OGR_FD_GetGeomFieldDefn(@feature_definition_pointer, index)
       return nil if field_ptr.null?
 
       OGR::Field.new(field_ptr)
@@ -104,7 +103,7 @@ module OGR
     # @param name [String]
     # @return [Fixnum]
     def geometry_field_index(name)
-      OGR_FD_GetGeomFieldIndex(@feature_definition_pointer, name)
+      FFI::GDAL.OGR_FD_GetGeomFieldIndex(@feature_definition_pointer, name)
     end
 
     # @param name [String]
@@ -118,7 +117,7 @@ module OGR
     def same?(other_feature_defintion)
       fd_ptr = GDAL._pointer(OGR::FeatureDefinition, other_feature_defintion)
 
-      OGR_FD_IsSame(@feature_definition_pointer, fd_ptr)
+      FFI::GDAL.OGR_FD_IsSame(@feature_definition_pointer, fd_ptr)
     end
     alias_method :==, :same?
   end
