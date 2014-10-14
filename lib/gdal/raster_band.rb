ac935a81e1a4a12491761c6d71139b9d32497f9d
Steve Loveless <steve@agrian.com>
2014-10-13 22:06:36 -0700

    RasterBand extension deletions got missed

--- lib/gdal/raster_band.rb
+++ lib/gdal/raster_band.rb
@@ -1,6 +1,6 @@
-require 'json'
 require_relative '../ffi/gdal'
 require_relative '../ffi/ogr/api_h'
+require_relative 'raster_band_extensions'
 require_relative 'color_table'
 require_relative 'major_object'
 require_relative 'raster_attribute_table'
@@ -9,6 +9,8 @@ require 'narray'
 module GDAL
   class RasterBand
     include MajorObject
+    include GDAL::Logger
+    include RasterBandExtensions
 
     # @param raster_band [GDAL::RasterBand, FFI::Pointer]
     def initialize(raster_band=nil)
@@ -188,12 +190,6 @@ module GDAL
       return nil if overview_pointer.null?
 
       self.class.new(overview_pointer)
-    end
-
-    def overviews
-      0.upto(overview_count - 1).map do |i|
-        overview(i)
-      end
     end
 
     # @param desired_samples [Fixnum] The returned band will have at least this
@@ -635,43 +631,6 @@ module GDAL
       { value: value, is_tight: is_tight.read_bytes(1).to_bool }
     end
 
-    # Iterates through all lines and builds an NArray of pixels.
-    #
-    # @return [NArray]
-    def to_na(width: nil, height: nil)
-      lines = []
-
-      readlines do |line|
-        lines << line
-      end
-
-      if height
-        rows_needed = height - lines.size
-
-        if rows_needed > 0
-          # create new empty lines at the end
-          rows_needed.times { lines << Array.new(lines.first.size, 0.0) }
-        elsif rows_needed < 0
-          # remove lines from the end
-          lines.pop(rows_needed.abs)
-        end
-      end
-
-      if width
-        columns_needed = width - lines.first.size
-
-        if columns_needed > 0
-          # create new empty lines at the end
-          lines.map! { |line| line.push(*Array.new(columns_needed, 0.0)) }
-        elsif rows_needed < 0
-          # remove lines from the end
-          lines.pop(rows_needed.abs)
-        end
-      end
-
-      NArray.to_na(lines)
-    end
-
     # Creates vector polygons for all connected regions of pixels in the raster
     # that share a common pixel value.
     #
@@ -704,41 +663,6 @@ module GDAL
       cpl_err.to_ruby
 
       OGR::Layer.new(layer_ptr)
-    end
-
-    # @return [Hash]
-    def as_json
-      {
-        raster_band: {
-          block_size: block_size,
-          category_names: category_names,
-          color_interpretation: color_interpretation,
-          color_table: color_table,
-          data_type: data_type,
-          default_histogram: default_histogram(true),
-          default_raster_attribute_table: default_raster_attribute_table,
-          has_arbitrary_overviews: arbitrary_overviews?,
-          mask_flags: mask_flags,
-          maximum_value: maximum_value,
-          minimum_value: minimum_value,
-          no_data_value: no_data_value,
-          number: number,
-          offset: offset,
-          overview_count: overview_count,
-          overviews: overviews,
-          scale: scale,
-          statistics: statistics,
-          unit_type: unit_type,
-          x_size: x_size,
-          y_size: y_size
-        },
-        metadata: all_metadata
-      }
-    end
-
-    # @return [String]
-    def to_json
-      as_json.to_json
     end
 
     #---------------------------------------------------------------------------
