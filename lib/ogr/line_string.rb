059e84a801e74ed1fb0ffc485d79b5a582a7a867
Steve Loveless <steve@agrian.com>
2015-01-21 19:32:12 -0800

    AGDEV-5963 Stupid github app

---
+++ lib/ogr/line_string.rb
@@ -0,0 +1,40 @@
+require_relative 'geometry'
+require_relative 'geometry_types/curve'
+
+module OGR
+  # A LineString is a type of Curve Geometry.
+  class LineString
+    include Geometry
+    include GeometryTypes::Curve
+
+    def self.approximate_arc_angles(center_x,
+                                    center_y,
+                                    z,
+                                    primary_radius,
+                                    secondary_radius,
+                                    rotation,
+                                    start_angle,
+                                    end_angle,
+                                    max_angle_step_size_degrees=0)
+      geometry_ptr = FFI::GDAL.OGR_G_ApproximateArcAngles(
+                      center_x,
+                      center_y,
+                      z,
+                      primary_radius,
+                      secondary_radius,
+                      rotation,
+                      start_angle,
+                      end_angle,
+                      max_angle_step_size_degrees
+                      )
+      return nil if geometry_ptr.null?
+
+      new(geometry_ptr)
+    end
+
+    def initialize(geometry_ptr=nil)
+      geometry_ptr ||= OGR::Geometry.create(:wkbLineString)
+      initialize_from_pointer(geometry_ptr)
+    end
+  end
+end
