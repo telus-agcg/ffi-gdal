82f30818aa0f26792628abb3a1a0ffdf44595fbf
Steve Loveless <steve@agrian.com>
2015-02-13 15:01:51 -0800

    AGDEV-6227 Allow for getting area_units from srs

--- lib/ogr/geometry_types/surface.rb
+++ lib/ogr/geometry_types/surface.rb
@@ -1,11 +1,19 @@
 module OGR
   module GeometryTypes
     module Surface
-      # Computes area for a LinearRing, Polygon, or MultiPolygon.
+      # Computes area for a LinearRing, Polygon, or MultiPolygon.  The area of
+      # the feature is in square units of the spatial reference system in use.
       #
       # @return [Float] 0.0 for unsupported geometry types.
       def area
         FFI::GDAL.OGR_G_Area(@geometry_pointer)
+      end
+
+      # Returns the units used by the associated OGR::SpatialReference.
+      #
+      # @return [Hash]
+      def area_units
+        spatial_reference ? spatial_reference.linear_units : nil
       end
 
       # Returns a point that's guaranteed to lie on the surface.
