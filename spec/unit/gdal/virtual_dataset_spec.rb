a58a8bb78687528f313dac53e536000ba44fcfff
Steve Loveless <steve@agrian.com>
2015-02-22 23:06:22 -0800

    AGDEV-6227 VRTs

---
+++ spec/unit/gdal/virtual_dataset_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+require 'gdal/virtual_dataset'
+require 'gdal/dataset'
+
+RSpec.describe GDAL::VirtualDataset do
+  subject(:virtual_dataset) { described_class.new(300, 200) }
+
+  describe '#initialize' do
+    it { is_expected.to be_a GDAL::VirtualDataset }
+  end
+
+  describe '#flush_cache' do
+    it 'does something' do
+      subject.flush_cache
+    end
+  end
+
+  describe '#to_xml' do
+    it 'returns a String' do
+      expect(subject.to_xml).to be_a String
+      expect(subject.to_xml.size).to be > 0
+    end
+  end
+
+  describe '#add_band' do
+    context 'valid data type' do
+      it "lets you add a real dataset's raster band to it" do
+        expect(subject.add_band(:GDT_Byte)).to eq 0
+      end
+    end
+  end
+end
