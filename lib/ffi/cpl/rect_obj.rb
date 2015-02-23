991e4e84dbe211d226608e2e881d42aacbbf3adb
Steve Loveless <steve@agrian.com>
2015-02-22 18:02:44 -0800

    AGDEV-6227 Added CPL QuadTree

---
+++ lib/ffi/cpl/rect_obj.rb
@@ -0,0 +1,12 @@
+require 'ffi'
+
+module FFI
+  module CPL
+    class RectObj < ::FFI::Struct
+      layout :min_x, :double,
+        :min_y, :double,
+        :max_x, :double,
+        :max_y, :double
+    end
+  end
+end
