6773152b9c358cf804534f4984bcc5957542f17d
Steve Loveless <steve@agrian.com>
2015-02-22 16:26:52 -0800

    AGDEV-6227 Added CPL HTTP functions

---
+++ lib/ffi/cpl/http.rb
@@ -0,0 +1,24 @@
+require 'ffi'
+require_relative 'http_result'
+
+module FFI
+  module CPL
+    module HTTP
+      extend ::FFI::Library
+      ffi_lib [FFI::CURRENT_PROCESS, FFI::GDAL.gdal_library_path]
+
+      #-------------------------------------------------------------------------
+      # Functions
+      #-------------------------------------------------------------------------
+      attach_function :CPLHTTPEnabled, [], :bool
+      attach_function :CPLHTTPFetch, %i[string pointer], HTTPResult.ptr
+      attach_function :CPLHTTPCleanup, [], :void
+      attach_function :CPLHTTPDestroyResult, [HTTPResult.ptr], :void
+      attach_function :CPLHTTPParseMultipartMime, [HTTPResult.ptr], :bool
+
+      attach_function :GOA2GetAuthorizationURL, %i[string], :string
+      attach_function :GOA2GetRefreshToken, %i[string string], :string
+      attach_function :GOA2GetAccessToken, %i[string string], :string
+    end
+  end
+end
