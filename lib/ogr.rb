f1c33b98c1650cf24dcb76646a0fde919233718c
Steve Loveless <steve@agrian.com>
2015-10-03 21:49:56 -0700

    AGDEV-8545 Autoload OGR geometry types

--- lib/ogr.rb
+++ lib/ogr.rb
@@ -5,4 +5,15 @@ module OGR
   include InternalHelpers
 
   FFI::OGR::API.OGRRegisterAll
+
+  autoload :GeometryCollection,     'ogr/geometries/geometry_collection'
+  autoload :LineString,             'ogr/geometries/line_string'
+  autoload :LinearRing,             'ogr/geometries/linear_ring'
+  autoload :MultiLineString,        'ogr/geometries/multi_line_string'
+  autoload :MultiPoint,             'ogr/geometries/multi_point'
+  autoload :MultiPolygon,           'ogr/geometries/multi_polygon'
+  autoload :NoneGeometry,           'ogr/geometries/none_geometry'
+  autoload :Point,                  'ogr/geometries/point'
+  autoload :Polygon,                'ogr/geometries/polygon'
+  autoload :UnknownGeometry,        'ogr/geometries/unknown_geometry'
 end
