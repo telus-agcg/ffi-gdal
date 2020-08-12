abe52ce47a3d6c16504ebf8910764d87677827f0
Steve Loveless <steve@agrian.com>
2020-08-11 17:25:18 -0700

    Add specs for GDAL::ColorEntry

--- spec/unit/gdal/color_entry_spec.rb
+++ spec/unit/gdal/color_entry_spec.rb
@@ -3,5 +3,69 @@
 require 'gdal/color_entry'
 
 RSpec.describe GDAL::ColorEntry do
-  pending 'Add some tests!'
+  describe '#c_pointer' do
+    it 'returns a FFI::Pointer' do
+      expect(subject.c_pointer).to be_a FFI::Pointer
+    end
+  end
+
+  describe '#color1' do
+    context 'default' do
+      it 'returns 0' do
+        expect(subject.color1).to eq 0
+      end
+    end
+  end
+
+  describe '#color1=' do
+    it 'sets the color1 attribute' do
+      subject.color1 = 20
+      expect(subject.color1).to eq 20
+    end
+  end
+
+  describe '#color2' do
+    context 'default' do
+      it 'returns 0' do
+        expect(subject.color2).to eq 0
+      end
+    end
+  end
+
+  describe '#color2=' do
+    it 'sets the color2 attribute' do
+      subject.color2 = 30
+      expect(subject.color2).to eq 30
+    end
+  end
+
+  describe '#color3' do
+    context 'default' do
+      it 'returns 0' do
+        expect(subject.color3).to eq 0
+      end
+    end
+  end
+
+  describe '#color3=' do
+    it 'sets the color3 attribute' do
+      subject.color3 = 40
+      expect(subject.color3).to eq 40
+    end
+  end
+
+  describe '#color4' do
+    context 'default' do
+      it 'returns 0' do
+        expect(subject.color4).to eq 0
+      end
+    end
+  end
+
+  describe '#color4=' do
+    it 'sets the color3 attribute' do
+      subject.color4 = 50
+      expect(subject.color4).to eq 50
+    end
+  end
 end
