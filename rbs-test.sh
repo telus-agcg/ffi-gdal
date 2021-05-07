73e54fb067b3415f715bc92c1024359eebf22cd6
Steve Loveless <steve.loveless@agrian.com>
2021-05-07 12:58:05 -0700

    Add tests/RBS defs for GDAL::EnvironmentMethods

--- rbs-test.sh
+++ rbs-test.sh
@@ -3,8 +3,8 @@
 set -e
 
 RBS_TEST_LOGLEVEL=error \
-  RBS_TEST_TARGET='GDAL::VersionInfo' \
+  RBS_TEST_TARGET='GDAL::*' \
   RBS_TEST_OPT='-rdate -I sig' \
   RBS_TEST_RAISE=true \
   RUBYOPT='-rbundler/setup -rrbs/test/setup' \
-  bundle exec rspec spec/unit/version_info_spec.rb
+  bundle exec rspec spec/unit/gdal
