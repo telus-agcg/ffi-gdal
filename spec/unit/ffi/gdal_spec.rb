20c3c8c953fb50615f8fc434746994bbb289a155
Steve Loveless <steve.loveless@agrian.com>
2022-06-10 13:29:56 -0700

    Remove FFI::GDAL.find_lib, .search_paths
    We should really just rely on FFI::Library.ffi_lib, but allow for using the GDAL_LIBRARY_PATH env var for overriding which gdal to use.

--- spec/unit/ffi/gdal_spec.rb
+++ spec/unit/ffi/gdal_spec.rb
@@ -4,48 +4,6 @@ require 'climate_control'
 require 'fakefs/safe'
 
 RSpec.describe FFI::GDAL do
-  describe '.find_lib' do
-    context 'ENV["GDAL_LIBRARY_PATH"] is set' do
-      around { |example| ClimateControl.modify(GDAL_LIBRARY_PATH: '/pants') { example.run } }
-
-      it 'returns GDAL_LIBRARY_PATH + libgdal file name' do
-        expect(Dir).to receive(:[]).with(%r{/pants/stuff\.+}).and_return ['/pants/stuff.dylib']
-        expect(described_class.find_lib('stuff')).to match %r{\A/pants/stuff\.+}
-      end
-    end
-
-    context 'ENV["GDAL_LIBRARY_PATH"] is not set' do
-      before do
-        allow(described_class).to receive(:search_paths).and_return %w[/pants]
-        FakeFS.activate!
-        FileUtils.mkdir 'pants'
-        FileUtils.touch("/pants/stuff.#{FFI::Platform::LIBSUFFIX}")
-      end
-
-      after { FakeFS.deactivate! }
-
-      it 'returns the search path + libgdal file name' do
-        expect(described_class.find_lib('stuff')).to match %r{\A/pants/stuff\.+}
-      end
-    end
-  end
-
-  describe '.search_paths' do
-    context 'ENV["GDAL_LIBRARY_PATH"] is set' do
-      around { |example| ClimateControl.modify(GDAL_LIBRARY_PATH: '/pants') { example.run } }
-
-      it 'returns GDAL_LIBRARY_PATH' do
-        expect(described_class.search_paths).to include %r{\A/pants\z}
-      end
-    end
-
-    context 'ENV["GDAL_LIBRARY_PATH"] is not set' do
-      it 'returns a bunch of directories' do
-        expect(described_class.search_paths.size).to be_positive
-      end
-    end
-  end
-
   describe '._files_with_constants' do
     it 'returns a non-empty Array' do
       expect(described_class._files_with_constants).to be_an Array
